03390eb493c29373449aefe8750421134358add7
Eugênio Cabral <eugfcl@gmail.com>
2018-08-11 21:46:34 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
+
 from setuptools import setup
+import sys
 import os
 
 try:
-   import pypandoc
-   long_description = pypandoc.convert('README.md', 'rst')
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
 except (IOError, ImportError):
-   long_description = open('README.md').read()
+    long_description = open('README.md').read()
+
+install_requires = [
+    'numpy',
+    'nltk',
+    'jellyfish',
+    'pycountry',
+    'newspaper;python_version<"3.0"',
+    'newspaper3k;python_version>="3.0"'
+]
 
 setup(name='geograpy',
       version='0.3.7',
@@ -17,15 +28,7 @@ setup(name='geograpy',
       author_email='jonathon@ushahidi.com',
       license='MIT',
       packages=['geograpy'],
-      install_requires=[
-            'numpy',
-            'nltk',
-            'newspaper',
-            'jellyfish',
-            'pycountry'
-      ],
+      install_requires=install_requires,
       scripts=['geograpy/bin/geograpy-nltk'],
-      package_data = {
-            'geograpy': ['data/*.csv'],
-      },
+      package_data = {'geograpy': ['data/*.csv'],},
       zip_safe=False)
